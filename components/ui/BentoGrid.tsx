9348ec35b93d23f8dc5816424a50ea8c975ec48b
spaceport <abidi.benhassen.a@gmail.com>
2024-08-07 15:42:50 +0100

    changes on projects and email button

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -184,7 +184,7 @@ export const BentoGridItem = ({
               </div>
 
               <MagicButton
-                title={copied ? "Email is Copied!" : "Copy my email address"}
+                title={copied ? "Email is Copied!" : "Copy my email"}
                 icon={<IoCopyOutline />}
                 position="left"
                 handleClick={handleCopy}
