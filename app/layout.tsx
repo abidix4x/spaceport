0f25ff70ba6d52e6992d59dcb72a840208601569
spaceport <abidi.benhassen.a@gmail.com>
2025-02-09 23:25:06 +0100

    changed the domain and name finally

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "spaceport",
+  title: "ABH",
   description: "The portal to Abidi's space",
 };
 
